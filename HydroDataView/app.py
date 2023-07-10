b0846acb2ca62615ae758c7c1b52cfaebdc0e487
Ryan Cohen <rcohenprogramming@gmail.com>
2023-07-10 12:58:17 -0400

    Started working on the pH chart in HydroDataView
    Had some troubles with highchart...

--- HydroDataView/app.py
+++ HydroDataView/app.py
@@ -5,8 +5,9 @@
 # charts and tables.
 #
 # Currently, nothing is displayed yet. The first view will be a pH chart.
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import mysql.connector, datetime
+from highcharts_stock import highcharts
 
 app = Flask(__name__)
 
@@ -15,6 +16,44 @@ PASSWORD = ''
 HOST = '127.0.0.1'
 DATABASE = 'testing'
 
+options = {
+    'chart': {
+        'type': 'spline'
+    },
+    'title': {
+        'text': 'pH Over Time'
+    },
+    'subtitle': {
+        'text': 'Irregular time data in Highcharts JS'
+    },
+    'xAxis': {
+        'type': 'datetime',
+        'title': {
+            'text': 'Date'
+        }
+    },
+    'yAxis': {
+        'title': {
+            'text': 'pH'
+        },
+        'min': 3
+    },
+    'tooltip': {
+        'headerFormat': '<b>{series.name}</b><br>',
+    },
+
+    'plotOptions': {
+        'series': {
+            'marker': {
+                'enabled': True,
+                'radius': 2.5
+            }
+        }
+    },
+
+    'colors': ['#6CF', '#39F', '#06C', '#036', '#000']
+}
+
 @app.route("/")
 def root_page():
     return "<p>KEEP GOING</>"
@@ -22,13 +61,43 @@ def root_page():
 
 @app.route("/status")
 def status_page():
-    return render_template('status.html', timestamp=datetime.datetime.now(), ph=6.12)
-
     cnx = mysql.connector.connect(user=USER, password=PASSWORD,
                                   host=HOST, database=DATABASE)
     cursor = cnx.cursor()
-    query = ("create table qtesting ( id int )")
+    query = ("SELECT timestamp, sensor_reading FROM sensor_readings")
     cursor.execute(query)
+
+    data = []
+    flags = []
+    i = 0
+    for (timestamp, sensor_reading) in cursor:
+        if i == 5:
+            flags.append({'y': sensor_reading, 'x': timestamp.strftime('%s'), 'title': 'FLAG', 'text': 'PH'})
+        data.append([timestamp, sensor_reading])
+        i += 1
     cursor.close()
     cnx.close()
-    return f"<p>TESTING</p>"
+
+    series = highcharts.SplineSeries(name = "pH Over Time", data = data, id='phseries')
+    #flags = highcharts.FlagsSeries(name="pH Flags", data_labels=flags, on_series='phseries')
+
+    # Fuck this crap that does not work.
+    # For "some reason" the python library for highcharts consistently removes all of my flags' data points
+    # UGH... Find another way to use highchart or use another chart framework
+    options['series'] = [{
+        'type': 'flags',
+        'name': 'FLAGS',
+        'onSeries': 'phseries',
+        'data': flags,
+        'shape': 'flag',
+        'title': 'ph down'
+    }];
+
+    chart = highcharts.Chart.from_options(options, chart_kwargs={'is_stock_chart': True})
+    chart.container = 'container'
+    chart.add_series(series)
+
+    chart.to_js_literal(filename='static/js/test.js')
+    
+    return render_template('status.html', chart_js=url_for('static', filename='js/test.js'))
+
