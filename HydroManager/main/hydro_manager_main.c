e8fee3e91c972dc938ea7f3e3c4aff111e6d4e90
Ryan Cohen <rcohenprogramming@gmail.com>
2023-07-15 10:33:36 -0400

    HydroManager: Get readings from BME280

--- HydroManager/main/hydro_manager_main.c
+++ HydroManager/main/hydro_manager_main.c
@@ -11,6 +11,7 @@
 #include <freertos/task.h>
 #include <driver/i2c.h>
 #include <ads111x.h>
+#include <bmp280.h>
 
 //------------------
 // Pin definitions
@@ -22,14 +23,22 @@
 #define I2C0_SDA 26
 #define I2C0_SCL 27
 
-// I2C Address for ADS1115 when it's connected to GND
+// I2C Address for ADS1115 when ADDR is connected to GND
 #define ADS1115_ADDR ADS111X_ADDR_GND
+// Use +-4.096v gain; there will be no signals above 3.3v or below 0v
 #define ADS1115_GAIN ADS111X_GAIN_4V096
 
+// I2C address for BME280 when SDO is connected to GND
+#define BME280_ADDR BMP280_I2C_ADDRESS_0
+
+// Stack size for each task
 #define STACK_SIZE 2048
 
 // I2C used for sensors
 i2c_dev_t i2c0_dev = {0};
+// BME280 temp/humidity/pressure sensor
+bmp280_t bme280_dev = {0};
+bmp280_params_t bme280_params = {0};
 
 SemaphoreHandle_t printf_sem;
 
@@ -69,6 +78,9 @@ void core0_loop(void *pvParameters) {
     for (;;) {
         ads1115_read(0);
         ads1115_read(1);
+        float pressure, temp, humidity;
+        ESP_ERROR_CHECK(bmp280_read_float(&bme280_dev, &temp, &pressure, &humidity));
+        TASK_PRINTF("T: %.2f C, P: %.2f Pa, H: %.2f %%\n", temp, pressure, humidity);
         TASK_PRINTF("-----------------------------\n");
         vTaskDelay(pdMS_TO_TICKS(500));
     }
@@ -104,6 +116,12 @@ void initialize_hardware() {
     ESP_ERROR_CHECK(ads111x_set_data_rate(&i2c0_dev, ADS111X_DATA_RATE_128));
     ESP_ERROR_CHECK(ads111x_set_input_mux(&i2c0_dev, ADS111X_MUX_0_GND));
     ESP_ERROR_CHECK(ads111x_set_gain(&i2c0_dev, ADS1115_GAIN));
+
+    // Initialize BME280 temp/humidity/pressure sensor
+    ESP_ERROR_CHECK(bmp280_init_desc(&bme280_dev, BME280_ADDR, I2C0_PORT, I2C0_SDA,
+                I2C0_SCL));
+    ESP_ERROR_CHECK(bmp280_init_default_params(&bme280_params));
+    ESP_ERROR_CHECK(bmp280_init(&bme280_dev, &bme280_params));
 
     printf("ADS1115 initialized.\n");
 }
